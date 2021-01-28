2f39c195b60bd18608d50539c6390d5b16c5581f
Aleksis Xenophon <aleksis.git@gmail.com>
2021-01-28 16:38:27 +0800

    (Update) Refactoring solution_serializer.py

--- nemde/core/model/serializers/solution_serializer.py
+++ nemde/core/model/serializers/solution_serializer.py
@@ -4,71 +4,15 @@ Serializer used to extract solution from Pyomo model and convert to JSON
 
 
 def get_region_total_dispatch(m, region_id, trade_type):
-    """Compute total dispatch in a given region rounded to two decimal places"""
+    """
+    Compute total dispatch in a given region rounded to two decimal places
+    """
 
     total = sum(m.V_TRADER_TOTAL_OFFER[i, j].value
                 for i, j in m.S_TRADER_OFFERS
                 if (j == trade_type) and (m.P_TRADER_REGION[i] == region_id))
 
     return total
-
-
-def get_region_solution(model, region_id):
-    """Extract solution for a given region"""
-
-    output = {
-        "@RegionID": region_id,
-        # "@PeriodID": "2020-11-01T04:05:00+10:00",
-        "@CaseID": model.P_CASE_ID.value,  # Not in NEMDE solution
-        "@Intervention": model.P_INTERVENTION_STATUS.value,
-        # "@EnergyPrice": "41.69967",
-        "@DispatchedGeneration": model.E_REGION_DISPATCHED_GENERATION[region_id].expr(),
-        "@DispatchedLoad": model.E_REGION_DISPATCHED_LOAD[region_id].expr(),
-        "@FixedDemand": model.E_REGION_FIXED_DEMAND[region_id].expr(),
-        "@NetExport": model.E_REGION_NET_EXPORT[region_id].expr(),
-        "@SurplusGeneration": model.V_CV_REGION_GENERATION_SURPLUS[region_id].value,
-        "@R6Dispatch": get_region_total_dispatch(m=model, region_id=region_id, trade_type='R6SE'),
-        "@R60Dispatch": get_region_total_dispatch(m=model, region_id=region_id, trade_type='R60S'),
-        "@R5Dispatch": get_region_total_dispatch(m=model, region_id=region_id, trade_type='R5MI'),
-        "@R5RegDispatch": get_region_total_dispatch(m=model, region_id=region_id, trade_type='R5RE'),
-        "@L6Dispatch": get_region_total_dispatch(m=model, region_id=region_id, trade_type='L6SE'),
-        "@L60Dispatch": get_region_total_dispatch(m=model, region_id=region_id, trade_type='L60S'),
-        "@L5Dispatch": get_region_total_dispatch(m=model, region_id=region_id, trade_type='L5MI'),
-        "@L5RegDispatch": get_region_total_dispatch(m=model, region_id=region_id, trade_type='L5RE'),
-        # "@R6Price": "1.49",
-        # "@R60Price": "1.73",
-        # "@R5Price": "0",
-        # "@R5RegPrice": "13.99",
-        # "@L6Price": "1.23",
-        # "@L60Price": "1.95",
-        # "@L5Price": "1.03",
-        # "@L5RegPrice": "3.75",
-        # "@AvailableGeneration": "8849",
-        # "@AvailableLoad": "0",
-        "@ClearedDemand": model.E_REGION_CLEARED_DEMAND[region_id].expr(),
-    }
-
-    return output
-
-
-def get_interconnector_solution(model, interconnector_id):
-    """Extract interconnector solution"""
-
-    output = {
-        "@InterconnectorID": interconnector_id,
-        # "@PeriodID": "2020-11-01T04:05:00+10:00",
-        "@CaseID": model.P_CASE_ID.value,  # Not in NEMDE solution
-        "@Intervention": model.P_INTERVENTION_STATUS.value,
-        "@Flow": model.V_GC_INTERCONNECTOR[interconnector_id].value,
-        "@Losses": model.V_LOSS[interconnector_id].value,
-        "@Deficit": model.V_CV_INTERCONNECTOR_REVERSE[interconnector_id].value,
-        # "@Price": "0",
-        # "@IdealLosses": "-0.59167",
-        # "@NPLExists": "0",
-        # "@InterRegionalLossFactor": "0.989524"
-    }
-
-    return output
 
 
 def get_trader_dispatch_target(model, trader_id, trade_type):
@@ -104,51 +48,6 @@ def get_trader_violation(model, trader_id, trade_type):
         return 0.0
 
 
-def get_trader_solution(model, trader_id):
-    """Extract solution for a given trader"""
-
-    energy_offer = get_energy_offer(model=model, trader_id=trader_id)
-
-    output = {
-        "@TraderID": trader_id,
-        # "@PeriodID": "2020-11-01T04:05:00+10:00",
-        "@CaseID": model.P_CASE_ID.value,  # Not in NEMDE solution
-        "@Intervention": model.P_INTERVENTION_STATUS.value,
-        "@EnergyTarget": get_trader_dispatch_target(model=model, trader_id=trader_id, trade_type=energy_offer),
-        "@R6Target": get_trader_dispatch_target(model=model, trader_id=trader_id, trade_type='R6SE'),
-        "@R60Target": get_trader_dispatch_target(model=model, trader_id=trader_id, trade_type='R60S'),
-        "@R5Target": get_trader_dispatch_target(model=model, trader_id=trader_id, trade_type='R5MI'),
-        "@R5RegTarget": get_trader_dispatch_target(model=model, trader_id=trader_id, trade_type='R5RE'),
-        "@L6Target": get_trader_dispatch_target(model=model, trader_id=trader_id, trade_type='L6SE'),
-        "@L60Target": get_trader_dispatch_target(model=model, trader_id=trader_id, trade_type='L60S'),
-        "@L5Target": get_trader_dispatch_target(model=model, trader_id=trader_id, trade_type='L5MI'),
-        "@L5RegTarget": get_trader_dispatch_target(model=model, trader_id=trader_id, trade_type='L5RE'),
-        # "@R6Price": "0",
-        # "@R60Price": "0",
-        # "@R5Price": "0",
-        # "@R5RegPrice": "0",
-        # "@L6Price": "0",
-        # "@L60Price": "0",
-        # "@L5Price": "0",
-        # "@L5RegPrice": "0",
-        "@R6Violation": get_trader_violation(model=model, trader_id=trader_id, trade_type='R6SE'),
-        "@R60Violation": get_trader_violation(model=model, trader_id=trader_id, trade_type='R60S'),
-        "@R5Violation": get_trader_violation(model=model, trader_id=trader_id, trade_type='R5MI'),
-        "@R5RegViolation": get_trader_violation(model=model, trader_id=trader_id, trade_type='R5RE'),
-        "@L6Violation": get_trader_violation(model=model, trader_id=trader_id, trade_type='L6SE'),
-        "@L60Violation": get_trader_violation(model=model, trader_id=trader_id, trade_type='L60S'),
-        "@L5Violation": get_trader_violation(model=model, trader_id=trader_id, trade_type='L5MI'),
-        "@L5RegViolation": get_trader_violation(model=model, trader_id=trader_id, trade_type='LRE'),
-        # "@FSTargetMode": "0",
-        # "@RampUpRate": "720",
-        # "@RampDnRate": "720",
-        # "@RampPrice": "0",
-        # "@RampDeficit": "0"
-    }
-
-    return output
-
-
 def get_constraint_deficit(model, constraint_id):
     """Get constraint ID"""
 
@@ -158,23 +57,6 @@ def get_constraint_deficit(model, constraint_id):
         return model.V_CV[constraint_id].value
     else:
         raise LookupError('Unrecognised constraint type:', model.P_GC_TYPE[constraint_id])
-
-
-def get_constraint_solution(model, constraint_id):
-    """Extract generic constraint solution"""
-
-    output = {
-        "@ConstraintID": constraint_id,
-        # "@Version": "20200817000000_1",
-        # "@PeriodID": "2020-11-01T04:05:00+10:00",
-        "@CaseID": model.P_CASE_ID.value,  # Not in NEMDE solution
-        # "@Intervention": "0",
-        "@RHS": model.P_GC_RHS[constraint_id],
-        # "@MarginalValue": "0",
-        "@Deficit": get_constraint_deficit(model=model, constraint_id=constraint_id)
-    }
-
-    return output
 
 
 def get_total_uigf_violation(model):
@@ -228,33 +110,6 @@ def get_total_fast_start_violation(model):
     return lhs + rhs + profile
 
 
-def get_case_solution(model):
-    """Extract case solution attributes"""
-
-    output = {
-        # "@SolverStatus": "0",
-        # "@Terminal": "NORREWMDS1A",
-        "@InterventionStatus": model.P_INTERVENTION_STATUS.value,
-        # "@SolverVersion": "3.3.15",
-        # "@NPLStatus": "0",
-        # "@TotalObjective": "-42158401.095",
-        # "@TotalAreaGenViolation": "0",
-        "@TotalInterconnectorViolation": get_total_interconnector_violation(model=model),
-        "@TotalGenericViolation": get_total_generic_constraint_violation(model=model),
-        "@TotalRampRateViolation": get_total_ramp_rate_violation(model=model),
-        "@TotalUnitMWCapacityViolation": get_total_unit_mw_capacity_violation(model=model),
-        # "@TotalEnergyConstrViolation": "0",
-        # "@TotalEnergyOfferViolation": "0",
-        # "@TotalASProfileViolation": "0",
-        "@TotalFastStartViolation": get_total_fast_start_violation(model=model),
-        # "@NumberOfDegenerateLPsSolved": "0",
-        "@TotalUIGFViolation": get_total_uigf_violation(model=model),
-        # "@OCD_Status": "Not_OCD"
-    }
-
-    return output
-
-
 def get_total_mnsp_ramp_rate_violation(model):
     """Get total MNSP ramp rate violation"""
 
@@ -276,30 +131,230 @@ def get_total_mnsp_capacity_violation(model):
     return sum(v.value for v in model.V_CV_MNSP_CAPACITY.values())
 
 
+def get_constraint_solution(model, constraint_id):
+    """Extract generic constraint solution"""
+
+    rhs = model.P_GC_RHS[constraint_id]
+    deficit = get_constraint_deficit(model=model, constraint_id=constraint_id)
+
+    output = {
+        "@ConstraintID": constraint_id,
+        # "@Version": "20200817000000_1",
+        # "@PeriodID": "2020-11-01T04:05:00+10:00",
+        "@CaseID": model.P_CASE_ID.value,  # Not in NEMDE solution
+        # "@Intervention": "0",
+        "@RHS": rhs,
+        # "@MarginalValue": "0",
+        "@Deficit": deficit
+    }
+
+    return output
+
+
+def get_trader_solution(model, trader_id):
+    """Extract solution for a given trader"""
+
+    # Energy offer target
+    energy_offer = get_energy_offer(model=model, trader_id=trader_id)
+    energy_target = get_trader_dispatch_target(
+        model=model, trader_id=trader_id, trade_type=energy_offer)
+
+    # FCAS targets
+    trade_types = ['R6SE', 'R60S', 'R5MI', 'R5RE', 'L6SE', 'L60S', 'L5MI', 'L5RE']
+    fcas = {
+        i: get_trader_dispatch_target(model=model, trader_id=trader_id, trade_type=i)
+        for i in trade_types
+    }
+
+    # FCAS violation
+    violation = {
+        i: get_trader_violation(model=model, trader_id=trader_id, trade_type=i)
+        for i in trade_types
+    }
+
+    output = {
+        "@TraderID": trader_id,
+        # "@PeriodID": "2020-11-01T04:05:00+10:00",
+        "@CaseID": model.P_CASE_ID.value,  # Not in NEMDE solution
+        "@Intervention": model.P_INTERVENTION_STATUS.value,
+        "@EnergyTarget": energy_target,
+        "@R6Target": fcas['R6SE'],
+        "@R60Target": fcas['R60S'],
+        "@R5Target": fcas['R5MI'],
+        "@R5RegTarget": fcas['R5RE'],
+        "@L6Target": fcas['L6SE'],
+        "@L60Target": fcas['L60S'],
+        "@L5Target": fcas['L5MI'],
+        "@L5RegTarget": fcas['L5RE'],
+        # "@R6Price": "0",
+        # "@R60Price": "0",
+        # "@R5Price": "0",
+        # "@R5RegPrice": "0",
+        # "@L6Price": "0",
+        # "@L60Price": "0",
+        # "@L5Price": "0",
+        # "@L5RegPrice": "0",
+        "@R6Violation": violation['R6SE'],
+        "@R60Violation": violation['R60S'],
+        "@R5Violation": violation['R5MI'],
+        "@R5RegViolation": violation['R5RE'],
+        "@L6Violation": violation['L6SE'],
+        "@L60Violation": violation['L60S'],
+        "@L5Violation": violation['L5MI'],
+        "@L5RegViolation": violation['L5RE'],
+        # "@FSTargetMode": "0",
+        # "@RampUpRate": "720",
+        # "@RampDnRate": "720",
+        # "@RampPrice": "0",
+        # "@RampDeficit": "0"
+    }
+
+    return output
+
+
+def get_region_solution(model, region_id):
+    """Extract solution for a given region"""
+
+    dispatched_generation = model.E_REGION_DISPATCHED_GENERATION[region_id].expr()
+    dispatched_load = model.E_REGION_DISPATCHED_LOAD[region_id].expr()
+    fixed_demand = model.E_REGION_FIXED_DEMAND[region_id].expr()
+    net_export = model.E_REGION_NET_EXPORT[region_id].expr()
+    surplus_generation = model.V_CV_REGION_GENERATION_SURPLUS[region_id].value
+    cleared_demand = model.E_REGION_CLEARED_DEMAND[region_id].expr()
+
+    # Total FCAS dispatch in each region
+    trade_types = ['R6SE', 'R60S', 'R5MI', 'R5RE', 'L6SE', 'L60S', 'L5MI', 'L5RE']
+    fcas = {i: get_region_total_dispatch(m=model, region_id=region_id, trade_type=i)
+            for i in trade_types}
+
+    output = {
+        "@RegionID": region_id,
+        # "@PeriodID": "2020-11-01T04:05:00+10:00",
+        "@CaseID": model.P_CASE_ID.value,  # Not in NEMDE solution
+        "@Intervention": model.P_INTERVENTION_STATUS.value,
+        # "@EnergyPrice": "41.69967",
+        "@DispatchedGeneration": dispatched_generation,
+        "@DispatchedLoad": dispatched_load,
+        "@FixedDemand": fixed_demand,
+        "@NetExport": net_export,
+        "@SurplusGeneration": surplus_generation,
+        "@R6Dispatch": fcas['R6SE'],
+        "@R60Dispatch": fcas['R60S'],
+        "@R5Dispatch": fcas['R5MI'],
+        "@R5RegDispatch": fcas['R5RE'],
+        "@L6Dispatch": fcas['L6SE'],
+        "@L60Dispatch": fcas['L60S'],
+        "@L5Dispatch": fcas['L5MI'],
+        "@L5RegDispatch": fcas['L5RE'],
+        # "@R6Price": "1.49",
+        # "@R60Price": "1.73",
+        # "@R5Price": "0",
+        # "@R5RegPrice": "13.99",
+        # "@L6Price": "1.23",
+        # "@L60Price": "1.95",
+        # "@L5Price": "1.03",
+        # "@L5RegPrice": "3.75",
+        # "@AvailableGeneration": "8849",
+        # "@AvailableLoad": "0",
+        "@ClearedDemand": cleared_demand
+    }
+
+    return output
+
+
+def get_interconnector_solution(model, interconnector_id):
+    """Extract interconnector solution"""
+
+    flow = model.V_GC_INTERCONNECTOR[interconnector_id].value
+    losses = model.V_LOSS[interconnector_id].value
+    deficit = model.V_CV_INTERCONNECTOR_REVERSE[interconnector_id].value
+
+    output = {
+        "@InterconnectorID": interconnector_id,
+        # "@PeriodID": "2020-11-01T04:05:00+10:00",
+        "@CaseID": model.P_CASE_ID.value,  # Not in NEMDE solution
+        "@Intervention": model.P_INTERVENTION_STATUS.value,
+        "@Flow": flow,
+        "@Losses": losses,
+        "@Deficit": deficit,
+        # "@Price": "0",
+        # "@IdealLosses": "-0.59167",
+        # "@NPLExists": "0",
+        # "@InterRegionalLossFactor": "0.989524"
+    }
+
+    return output
+
+
+def get_case_solution(model):
+    """Extract case solution attributes"""
+
+    interconnector_violation = get_total_interconnector_violation(model=model)
+    generic_constraint_violation = get_total_generic_constraint_violation(model=model)
+    ramp_rate_violation = get_total_ramp_rate_violation(model=model)
+    unit_mw_capacity_violation = get_total_unit_mw_capacity_violation(model=model)
+    fast_start_violation = get_total_fast_start_violation(model=model)
+    uigf_violation = get_total_uigf_violation(model=model)
+
+    output = {
+        # "@SolverStatus": "0",
+        # "@Terminal": "NORREWMDS1A",
+        "@InterventionStatus": model.P_INTERVENTION_STATUS.value,
+        # "@SolverVersion": "3.3.15",
+        # "@NPLStatus": "0",
+        # "@TotalObjective": "-42158401.095",
+        # "@TotalAreaGenViolation": "0",
+        "@TotalInterconnectorViolation": interconnector_violation,
+        "@TotalGenericViolation": generic_constraint_violation,
+        "@TotalRampRateViolation": ramp_rate_violation,
+        "@TotalUnitMWCapacityViolation": unit_mw_capacity_violation,
+        # "@TotalEnergyConstrViolation": "0",
+        # "@TotalEnergyOfferViolation": "0",
+        # "@TotalASProfileViolation": "0",
+        "@TotalFastStartViolation": fast_start_violation,
+        # "@NumberOfDegenerateLPsSolved": "0",
+        "@TotalUIGFViolation": uigf_violation,
+        # "@OCD_Status": "Not_OCD"
+    }
+
+    return output
+
+
 def get_period_solution(model):
     """Extract period solution"""
+
+    objective = model.OBJECTIVE.expr()
+    interconnector_violation = get_total_interconnector_violation(model=model)
+    generic_constraint_violation = get_total_generic_constraint_violation(model=model)
+    ramp_rate_violation = get_total_ramp_rate_violation(model=model)
+    unit_mw_capacity_violation = get_total_unit_mw_capacity_violation(model=model)
+    fast_start_violation = get_total_fast_start_violation(model=model)
+    mnsp_ramp_rate_violation = get_total_mnsp_ramp_rate_violation(model=model)
+    mnsp_offer_violation = get_total_mnsp_offer_violation(model=model)
+    mnsp_capacity_violation = get_total_mnsp_capacity_violation(model=model)
+    uigf_violation = get_total_uigf_violation(model=model)
 
     output = {
         # "@PeriodID": "2020-11-01T04:05:00+10:00",
         "@CaseID": model.P_CASE_ID.value,  # Not in NEMDE solution
         "@Intervention": model.P_INTERVENTION_STATUS.value,
         # "@SwitchRunBestStatus": "1",
-        "@TotalObjective": model.OBJECTIVE.expr(),
+        "@TotalObjective": objective,
         # "@SolverStatus": "0",
         # "@NPLStatus": "0",
         # "@TotalAreaGenViolation": "0",
-        "@TotalInterconnectorViolation": get_total_interconnector_violation(model=model),
-        "@TotalGenericViolation": get_total_generic_constraint_violation(model=model),
-        "@TotalRampRateViolation": get_total_ramp_rate_violation(model=model),
-        "@TotalUnitMWCapacityViolation": get_total_unit_mw_capacity_violation(model=model),
+        "@TotalInterconnectorViolation": interconnector_violation,
+        "@TotalGenericViolation": generic_constraint_violation,
+        "@TotalRampRateViolation": ramp_rate_violation,
+        "@TotalUnitMWCapacityViolation": unit_mw_capacity_violation,
         # "@TotalEnergyConstrViolation": "0",
         # "@TotalEnergyOfferViolation": "0",
         # "@TotalASProfileViolation": "0",
-        "@TotalFastStartViolation": get_total_fast_start_violation(model=model),
-        "@TotalMNSPRampRateViolation": get_total_mnsp_ramp_rate_violation(model=model),
-        "@TotalMNSPOfferViolation": get_total_mnsp_offer_violation(model=model),
-        "@TotalMNSPCapacityViolation": get_total_mnsp_capacity_violation(model=model),
-        "@TotalUIGFViolation": get_total_uigf_violation(model=model)
+        "@TotalFastStartViolation": fast_start_violation,
+        "@TotalMNSPRampRateViolation": mnsp_ramp_rate_violation,
+        "@TotalMNSPOfferViolation": mnsp_offer_violation,
+        "@TotalMNSPCapacityViolation": mnsp_capacity_violation,
+        "@TotalUIGFViolation": uigf_violation
     }
 
     return output
@@ -311,8 +366,8 @@ def get_solution(model, format=None):
     output = {
         'CaseSolution': get_case_solution(model=model),
         'PeriodSolution': get_period_solution(model=model),
-        'RegionSolution': [get_region_solution(model=model, region_id=r) for r in model.S_REGIONS],
-        'TraderSolution': [get_trader_solution(model=model, trader_id=t) for t in model.S_TRADERS],
+        'RegionSolution': [get_region_solution(model=model, region_id=i) for i in model.S_REGIONS],
+        'TraderSolution': [get_trader_solution(model=model, trader_id=i) for i in model.S_TRADERS],
         'InterconnectorSolution': [get_interconnector_solution(model=model, interconnector_id=i) for i in model.S_INTERCONNECTORS],
         'ConstraintSolution': [get_constraint_solution(model=model, constraint_id=i) for i in model.S_GENERIC_CONSTRAINTS],
     }
