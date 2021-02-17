4648e7e36475d30e87e6c1f5c3e006c865a5800a
Aleksis Xenophon <aleksis.git@gmail.com>
2021-02-17 20:39:57 +0800

    (Update) Copying junitxml file from remote machine

--- scripts/copy_reports.sh
+++ scripts/copy_reports.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-gcloud compute scp --recurse vm-4-p:~/nemde/reports/*.zip ~/Desktop/repos/projects/nemde-dev/nemde/reports
+
+gcloud compute scp --recurse vm-4-p:~/nemde/reports/*.zip ~/Desktop/repos/projects/nemde-dev/nemde/reports/
+gcloud compute scp --recurse vm-4-p:~/nemde/reports/latest.xml ~/Desktop/repos/projects/nemde-dev/nemde/reports/latest.xml
