b011d3dc5c74f71b8f3873be6904318870769364
Aleksis Xenophon <aleksis.git@gmail.com>
2021-06-02 10:12:05 +0800

    (Update) Adding instance region to command line argument

--- scripts/copy_reports.sh
+++ scripts/copy_reports.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gcloud compute scp --recurse nemde-backtest-p:~/nemde/reports/*.zip ~/Desktop/repos/projects/nemde-dev/nemde/reports/
-gcloud compute scp --recurse nemde-backtest-p:~/nemde/reports/latest.xml ~/Desktop/repos/projects/nemde-dev/nemde/reports/latest.xml
+gcloud compute scp --recurse nemde-backtest-p:~/nemde/reports/*.zip ~/Desktop/repos/projects/nemde-dev/nemde/reports/ --zone=asia-east1-b
+gcloud compute scp --recurse nemde-backtest-p:~/nemde/reports/latest.xml ~/Desktop/repos/projects/nemde-dev/nemde/reports/latest.xml --zone=asia-east1-b
